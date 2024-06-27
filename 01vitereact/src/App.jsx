18dac00d0f359ee54daaab5472bd3a18b6db017c
Animesh Ojha <animeshojha44@gmail.com>
2024-06-27 22:04:08 +0530

    react flow done

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,8 +1,9 @@
-
+import Chai from "./chai"
 function App() {
 
   return (
     <>
+     <Chai />
       <h1>hello my name is animesh ojha and i m learning react at this time</h1>
     </>
   )
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,6 @@
+function Chai(){
+    return(
+        <h1>chai aur code</h1>
+    )
+}
+export default Chai;
